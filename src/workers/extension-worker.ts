4e93a1e9ad7cfc4184fb83199e9403dcd890853a
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-02-19 18:09:38 -0800

    v2 poc

--- src/workers/extension-worker.ts
+++ src/workers/extension-worker.ts
@@ -1,102 +1,64 @@
-import { Observable, debounceTime } from "rxjs";
+import { Subject, filter, groupBy, map, merge, mergeMap, scan } from "rxjs";
 
+const $tabUpdated = new Subject<{ tabId: number; changeInfo: chrome.tabs.TabChangeInfo; tab: chrome.tabs.Tab }>();
+const $tabCreated = new Subject<chrome.tabs.Tab>();
+
+// v2
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => $tabUpdated.next({ tabId, changeInfo, tab }));
+chrome.tabs.onCreated.addListener((tab) => $tabCreated.next(tab));
+
+// v1
 chrome.action.onClicked.addListener(handleActionClick);
 chrome.runtime.onInstalled.addListener(handleExtensionInstall);
 chrome.runtime.onStartup.addListener(handleBrowserStart);
 chrome.commands.onCommand.addListener(handleCommand);
-chrome.tabs.onCreated.addListener((tab) => {
-  console.log({ created: tab });
-  // immediate move the opener tab and its group to the front
+
+const $tabUpsert = merge(
+  $tabUpdated,
+  $tabCreated.pipe(map((tab) => ({ tabId: tab.id!, changeInfo: { status: "created" }, tab }))),
+);
+const $tabUpdatedPerTab = $tabUpsert.pipe(
+  groupBy((update) => update.tabId),
+  map(($group) =>
+    $group.pipe(filter((update) => ["created", "loading", "complete"].includes(update.changeInfo.status as string))),
+  ),
+);
+
+const $tabLifecycle = $tabUpdatedPerTab.pipe(
+  mergeMap((tab) => {
+    return tab.pipe(
+      scan(
+        (acc, update) => {
+          const baseAcc = acc.some((update: any) => update.status === "complete") ? [] : acc;
+          return [...baseAcc, { tab: update.tab, status: update.changeInfo.status as string }];
+        },
+        [] as { tab: chrome.tabs.Tab; status: string }[],
+      ),
+    );
+  }),
+);
+
+// on created, move openner
+$tabCreated.subscribe(async (tab) => {
+  const openerId = tab.openerTabId;
+  if (!openerId) return;
+
+  const openerTab = await chrome.tabs.get(openerId);
+  if (!openerTab.id) return;
+
+  chrome.tabs.move(openerTab.id, { index: 0 });
 });
 
-chrome.tabs.onUpdated.addListener(handleTabUpdated);
-
-// IDEA use openerId to build a tree of navigation stops, apply to active group only
-// IDEA when an active tab initiates a navigation, push the tab and group to the front, no rotation
-
-const $tabHighlighted = new Observable<chrome.tabs.TabHighlightInfo>((subscriber) => {
-  // assumption 1: only a single subscriber
-  // assumption 2: will never unsubscribe
-  chrome.tabs.onHighlighted.addListener(() => subscriber.next());
+// on loading and complete, move tab itself
+$tabUpdated.subscribe(async (update) => {
+  if (!update.tabId) return;
+  if (!update.tab.active) return;
+  chrome.tabs.move(update.tabId, { index: 0 });
 });
 
-$tabHighlighted.pipe(debounceTime(2000)).subscribe(handleTabHighlighted);
-
-async function handleTabHighlighted(_highlightInfo: chrome.tabs.TabHighlightInfo) {
-  const highlightedTab = await chrome.tabs.query({ currentWindow: true, highlighted: true }).then((tabs) => tabs[0]);
-
-  // rotate highlighted tab group to the front of the window
-  const tabs = await chrome.tabs.query({ currentWindow: true });
-  const groups = [...groupToMapBy(tabs, (tab) => tab.groupId).entries()].map(([groupId, tabs]) => ({
-    groupId,
-    tabs: tabs ?? [],
-    highlighted: (tabs ?? []).some((tab) => tab.highlighted),
-  }));
-
-  const groupsBeforeHighlighted = groups.slice(
-    0,
-    groups.findIndex((group) => group.highlighted),
-  );
-
-  await Promise.allSettled(groupsBeforeHighlighted.map((group) => chrome.tabGroups.move(group.groupId, { index: -1 })));
-
-  // rotate highlighted tab to the front of the group
-
-  const tabsInGroup = await chrome.tabs.query({ currentWindow: true, groupId: highlightedTab.groupId });
-  const tabsBeforeHighlighted = tabsInGroup.slice(
-    0,
-    tabsInGroup.findIndex((tab) => tab.highlighted),
-  );
-
-  await Promise.allSettled(
-    tabsBeforeHighlighted.map((tab) => chrome.tabs.move(tab.id!, { index: tabsInGroup.length - 1 })),
-  );
-}
-
-async function handleTabUpdated(tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) {
-  console.log({ updated: tab });
-
-  if (changeInfo.status !== "complete") return; // if user is dragging
-  console.log(`[worker] tab updated [${tabId}]: ${changeInfo.status} ${tab.url}`);
-  if (!tab.url) return; // TBD: do we need to remove tabs e.g. when current tab is replaced by blank url?
-
-  // TODO New Tab Page cannot be removed
-  const identicalTab = await findIdenticalTab(tab);
-  if (identicalTab) {
-    console.log(`[worker] found identical tab [${identicalTab.id}]: ${identicalTab.url}`);
-    await chrome.tabs.remove(tabId);
-    await chrome.tabs.highlight({ tabs: identicalTab.index });
-    return;
-  }
-
-  const pageKey = await getPageKey(tab.url);
-  console.log(tab.url, pageKey);
-
-  const [identitySharingTabs, groupSharingTabs] = await Promise.all([
-    findTabsByGroupIdentity(pageKey),
-    getTabsByGroupId(tab.groupId),
-  ]);
-
-  const uniqueHostKeys = await Promise.all(groupSharingTabs.map((tab) => tab.url ?? "").map((url) => getPageKey(url)));
-  const uniqueHostsInGroup = new Set(uniqueHostKeys);
-  const invalidGroupIds = new Set<number>([chrome.tabGroups.TAB_GROUP_ID_NONE]);
-  if (uniqueHostsInGroup.size > 1) invalidGroupIds.add(tab.groupId);
-
-  // if the same host is split across multiple groups, avoid adding to the current group
-  const uniqueGroups = new Set(identitySharingTabs.filter(isGroupedTab).map((tab) => tab.groupId));
-  if (uniqueGroups.size > 1) invalidGroupIds.add(tab.groupId);
-
-  const validIdentitySharingTabs = identitySharingTabs.filter((tab) => !invalidGroupIds.has(tab.groupId));
-  const newGroupId = validIdentitySharingTabs.at(-1)?.groupId;
-
-  /* move tab to group front */
-  if (tab.highlighted) {
-    await chrome.tabs.move(tabId, { index: 0 });
-  }
-  const groupId = await chrome.tabs.group({ tabIds: [tabId], groupId: newGroupId });
-
-  chrome.tabGroups.update(groupId, { title: getGroupTitle(identitySharingTabs) });
-}
+$tabLifecycle.subscribe((updates) => {
+  console.log(updates);
+});
 
 async function handleCommand(command: string) {
   switch (command) {
@@ -243,4 +205,79 @@ function groupToMapBy<T, K extends string | number>(array: T[], key: (item: T) =
   }, new Map<K, T[]>());
 
   return map;
+}
+
+// TODO clean up
+async function handleTabHighlighted(_highlightInfo: chrome.tabs.TabHighlightInfo) {
+  const highlightedTab = await chrome.tabs.query({ currentWindow: true, highlighted: true }).then((tabs) => tabs[0]);
+
+  // rotate highlighted tab group to the front of the window
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  const groups = [...groupToMapBy(tabs, (tab) => tab.groupId).entries()].map(([groupId, tabs]) => ({
+    groupId,
+    tabs: tabs ?? [],
+    highlighted: (tabs ?? []).some((tab) => tab.highlighted),
+  }));
+
+  const groupsBeforeHighlighted = groups.slice(
+    0,
+    groups.findIndex((group) => group.highlighted),
+  );
+
+  await Promise.allSettled(groupsBeforeHighlighted.map((group) => chrome.tabGroups.move(group.groupId, { index: -1 })));
+
+  // rotate highlighted tab to the front of the group
+
+  const tabsInGroup = await chrome.tabs.query({ currentWindow: true, groupId: highlightedTab.groupId });
+  const tabsBeforeHighlighted = tabsInGroup.slice(
+    0,
+    tabsInGroup.findIndex((tab) => tab.highlighted),
+  );
+
+  await Promise.allSettled(
+    tabsBeforeHighlighted.map((tab) => chrome.tabs.move(tab.id!, { index: tabsInGroup.length - 1 })),
+  );
+}
+
+async function handleTabUpdated(tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) {
+  if (changeInfo.status !== "complete") return; // if user is dragging
+  console.log(`[worker] tab updated [${tabId}]: ${changeInfo.status} ${tab.url}`);
+  if (!tab.url) return; // TBD: do we need to remove tabs e.g. when current tab is replaced by blank url?
+
+  // TODO New Tab Page cannot be removed
+  const identicalTab = await findIdenticalTab(tab);
+  if (identicalTab) {
+    console.log(`[worker] found identical tab [${identicalTab.id}]: ${identicalTab.url}`);
+    await chrome.tabs.remove(tabId);
+    await chrome.tabs.highlight({ tabs: identicalTab.index });
+    return;
+  }
+
+  const pageKey = await getPageKey(tab.url);
+  console.log(tab.url, pageKey);
+
+  const [identitySharingTabs, groupSharingTabs] = await Promise.all([
+    findTabsByGroupIdentity(pageKey),
+    getTabsByGroupId(tab.groupId),
+  ]);
+
+  const uniqueHostKeys = await Promise.all(groupSharingTabs.map((tab) => tab.url ?? "").map((url) => getPageKey(url)));
+  const uniqueHostsInGroup = new Set(uniqueHostKeys);
+  const invalidGroupIds = new Set<number>([chrome.tabGroups.TAB_GROUP_ID_NONE]);
+  if (uniqueHostsInGroup.size > 1) invalidGroupIds.add(tab.groupId);
+
+  // if the same host is split across multiple groups, avoid adding to the current group
+  const uniqueGroups = new Set(identitySharingTabs.filter(isGroupedTab).map((tab) => tab.groupId));
+  if (uniqueGroups.size > 1) invalidGroupIds.add(tab.groupId);
+
+  const validIdentitySharingTabs = identitySharingTabs.filter((tab) => !invalidGroupIds.has(tab.groupId));
+  const newGroupId = validIdentitySharingTabs.at(-1)?.groupId;
+
+  /* move tab to group front */
+  if (tab.highlighted) {
+    await chrome.tabs.move(tabId, { index: 0 });
+  }
+  const groupId = await chrome.tabs.group({ tabIds: [tabId], groupId: newGroupId });
+
+  chrome.tabGroups.update(groupId, { title: getGroupTitle(identitySharingTabs) });
 }
