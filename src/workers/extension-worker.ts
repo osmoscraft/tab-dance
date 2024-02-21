474e33068ebd56e16ee6165fada5f88cf7521959
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-02-21 12:08:26 -0800

    update next steps design

--- src/workers/extension-worker.ts
+++ src/workers/extension-worker.ts
@@ -2,8 +2,10 @@ chrome.commands.onCommand.addListener(handleCommand);
 chrome.action.onClicked.addListener(handleActionClick);
 chrome.runtime.onInstalled.addListener(handleExtensionInstall);
 
+// TODO audio feedback for open in new tab
 // TODO audio feedback for tab closure
 // TODO audio feedback for dive-in
+// TODO audio feedback for dive-out
 // TODO options page to allow audio feedback
 
 async function handleCommand(command: string) {
